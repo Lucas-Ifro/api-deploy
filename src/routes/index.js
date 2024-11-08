f969c5610a277cd1475a7c89c35dd556dd0bc38c
lucas Ferreira <lucaslfr00@gmail.com>
2024-11-08 12:47:27 -0300

    atualizando

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,40 @@
-// src/routes/index.js
-export default (app) => {
-    app.get('/', (req, res) => {
-      res.send('Olá, mundo!');
+import express from "express";
+import imagens from "./ImagensRoute.js"
+import users from "./UsuarioRoutes.js";
+import login from "./loginRoute.js";
+import turma from "./TurmaRoutes.js";
+import aula from "./AulaRoute.js";
+import modulo from "./ModuloRoutes.js"
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
+import getSwaggerOptions from "../docs/config/head.js";
+
+const routes = (app) => {
+
+
+    // Configurando a documentação da Swagger UI para ser servida diretamente em '/'
+    const swaggerDocs = swaggerJsDoc(getSwaggerOptions());
+    app.use(swaggerUI.serve);
+    app.get("/", (req, res, next) => {
+        swaggerUI.setup(swaggerDocs)(req, res, next);
     });
-  
-    // Defina outras rotas aqui, por exemplo:
-    app.get('/users', (req, res) => {
-      res.json({ users: [] });
+
+    app.use(imagens);
+
+    app.use(express.json(),
+        // rotas para autentição e autorização (permissão)
+        login,
+        turma,
+        aula,
+        users,
+        modulo
+
+    );
+
+    // Se não é nenhuma rota válida, produz 404
+    app.use((req, res, next) => {
+        res.status(404).json({ message: "Rota não encontrada" });
     });
-  };
-  
+};
+
+export default routes;
