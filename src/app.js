99917fedf1bc1ba19374b1b79c5f14014fd80325
lucas Ferreira <lucaslfr00@gmail.com>
2024-11-08 12:25:12 -0300

    atualizando

--- src/app.js
+++ src/app.js
@@ -19,5 +19,13 @@ app.use(express.json());
 // Passando para o arquivo de rotas o app, que envia junto uma instância do express
 routes(app);
 
+const port = process.env.PORT || 3051;
+
+app.listen(port, () => {
+    console.log(`Servidor escutando em http://localhost:${port}`)
+})
+
 // exportando para o server.js fazer uso
 export default app
+
+
